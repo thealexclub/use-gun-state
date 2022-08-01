e259e23d2a71a3486e7b97e7cee3c4e1ef7752d7
Alex Litzow <enter@thealex.club>
2022-08-01 19:23:13 +1000

    chat-app.js

---
+++ examples/chat-app.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function ChatApp() {
+  return (
+    <div className="chat-app">
+      <div className="chat-app__messages">
+        <Message
+          id="janhaldjha"
+          userName="Alex Yeah"
+          body="This is a message"
+          timeSent={Date.now().toString()}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Message({ userName, timeSent, body }) {
+  const time = new Date(timeSent);
+
+  return (
+    <div className="message">
+      <div className="message__header">
+        <div className="m-h__time">{time.toTimeString()}</div>
+        <div className="m-h__user">{userName}</div>
+      </div>
+      <p className="message__body">{body}</p>
+    </div>
+  );
+}
